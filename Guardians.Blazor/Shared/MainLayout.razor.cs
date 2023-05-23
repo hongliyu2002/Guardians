d3d6f26c79a0359ae0398a75081c3092e6adb546
Leo Hong <hongliyu2002@hotmail.com>
2023-05-23 21:25:50 +0800

    Update

---
+++ Guardians.Blazor/Shared/MainLayout.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using MudBlazor;
+
+namespace Guardians.Blazor;
+
+public partial class MainLayout
+{
+    [Inject]
+    private IDialogService DialogService { get; set; } = default!;
+
+    private bool _drawerOpen = true;
+    private Anchor _drawerAnchor;
+
+    private void DrawerToggle()
+    {
+        _drawerOpen = !_drawerOpen;
+    }
+    
+    private void ToggleDrawerAnchor()
+    {
+        _drawerAnchor = _drawerAnchor == Anchor.Start ? Anchor.End : Anchor.Start;
+    }
+}
