4788e3b09f316ee818e0a4d3a4eaad594dd6c7e4
Leo Hong <hongliyu2002@hotmail.com>
2023-05-24 00:14:34 +0800

    Update

--- Guardians.Blazor/Shared/MainLayout.razor.cs
+++ Guardians.Blazor/Shared/MainLayout.razor.cs
@@ -7,17 +7,4 @@ public partial class MainLayout
 {
     [Inject]
     private IDialogService DialogService { get; set; } = default!;
-
-    private bool _drawerOpen = true;
-    private Anchor _drawerAnchor;
-
-    private void DrawerToggle()
-    {
-        _drawerOpen = !_drawerOpen;
-    }
-    
-    private void ToggleDrawerAnchor()
-    {
-        _drawerAnchor = _drawerAnchor == Anchor.Start ? Anchor.End : Anchor.Start;
-    }
 }
