3b9d9e03d6692d2badcc344fc32812a86a46e693
liyuhong <liyuhong@tesla.com>
2023-05-25 09:58:03 +0800

    Update

--- Guardians.Blazor/GuardiansBlazorModule.cs
+++ Guardians.Blazor/GuardiansBlazorModule.cs
@@ -2,12 +2,10 @@
 using Fluxera.Extensions.Hosting.Modules;
 using Fluxera.Extensions.Hosting.Modules.AspNetCore;
 using Fluxera.Extensions.Hosting.Modules.AspNetCore.HealthChecks;
-using Guardians.Application;
 using Guardians.AspNetCore.Components.Server;
 using Guardians.Blazor.ViewModels;
-using Guardians.Infrastructure.Contexts;
+using Guardians.HttpClient;
 using JetBrains.Annotations;
-using Microsoft.EntityFrameworkCore;
 using MudBlazor.Services;
 
 namespace Guardians.Blazor;
@@ -15,7 +13,7 @@ namespace Guardians.Blazor;
 [PublicAPI]
 [DependsOn<ComponentsServerModule>]
 [DependsOn<HealthChecksEndpointsModule>]
-[DependsOn<GuardiansApplicationModule>]
+[DependsOn<GuardiansHttpClientModule>]
 public sealed class GuardiansBlazorModule : ConfigureApplicationModule
 {
     /// <inheritdoc />
@@ -24,7 +22,7 @@ public sealed class GuardiansBlazorModule : ConfigureApplicationModule
         context.Log("AddMudServices", services => services.AddMudServices());
         context.Log("AddReportViewModel", services => services.AddScoped<ReportViewModel>());
     }
-    
+
     /// <inheritdoc />
     public override void Configure(IApplicationInitializationContext context)
     {
