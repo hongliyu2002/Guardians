83d01411e34abb90574bc80272497b3e0ae28770
liyuhong <liyuhong@tesla.com>
2023-05-25 11:03:53 +0800

    Update

--- Guardians.HttpClient/Services/CaseApplicationServiceClient.cs
+++ Guardians.HttpClient/Services/CaseApplicationServiceClient.cs
@@ -19,57 +19,99 @@ internal sealed class CaseApplicationServiceClient : HttpClientServiceBase, ICas
     /// <inheritdoc />
     public async Task<ResultDto<CaseDto>> CreateCaseAsync(CaseForCreationDto input)
     {
-        var content = input.AsJsonContent();
-        var response = await HttpClient.PostAsync("/api/cases", content);
-        var result = await response.Content.ReadAsAsync<ResultDto<CaseDto>>();
-        return result;
+        try
+        {
+            var content = input.AsJsonContent();
+            var response = await HttpClient.PostAsync("/api/cases", content);
+            var result = await response.Content.ReadAsAsync<ResultDto<CaseDto>>();
+            return result;
+        }
+        catch (Exception ex)
+        {
+            return new ResultDto<CaseDto> { Code = 500, Msg = ex.Message };
+        }
     }
 
     /// <inheritdoc />
     public async Task<ResultDto<CaseDto>> UpdateCaseInfoAsync(CaseId caseID, CaseForUpdateDto input)
     {
-        var content = input.AsJsonContent();
-        var response = await HttpClient.PutAsync($"/api/cases/{caseID}", content);
-        var result = await response.Content.ReadAsAsync<ResultDto<CaseDto>>();
-        return result;
+        try
+        {
+            var content = input.AsJsonContent();
+            var response = await HttpClient.PutAsync($"/api/cases/{caseID}", content);
+            var result = await response.Content.ReadAsAsync<ResultDto<CaseDto>>();
+            return result;
+        }
+        catch (Exception ex)
+        {
+            return new ResultDto<CaseDto> { Code = 500, Msg = ex.Message };
+        }
     }
 
     /// <inheritdoc />
     public async Task<ResultDto<CaseDto>> ChangeCaseStatusAsync(CaseId caseID, CaseForStatusChangeDto input)
     {
-        var content = input.AsJsonContent();
-        var response = await HttpClient.PutAsync($"/api/cases/{caseID}/status", content);
-        var result = await response.Content.ReadAsAsync<ResultDto<CaseDto>>();
-        return result;
+        try
+        {
+            var content = input.AsJsonContent();
+            var response = await HttpClient.PutAsync($"/api/cases/{caseID}/status", content);
+            var result = await response.Content.ReadAsAsync<ResultDto<CaseDto>>();
+            return result;
+        }
+        catch (Exception ex)
+        {
+            return new ResultDto<CaseDto> { Code = 500, Msg = ex.Message };
+        }
     }
 
     /// <inheritdoc />
     public async Task<ResultDto<CaseId>> DeleteCaseAsync(CaseId caseID)
     {
-        var response = await HttpClient.DeleteAsync($"/api/cases/{caseID}");
-        var result = await response.Content.ReadAsAsync<ResultDto<CaseId>>();
-        return result;
+        try
+        {
+            var response = await HttpClient.DeleteAsync($"/api/cases/{caseID}");
+            var result = await response.Content.ReadAsAsync<ResultDto<CaseId>>();
+            return result;
+        }
+        catch (Exception ex)
+        {
+            return new ResultDto<CaseId> { Code = 500, Msg = ex.Message };
+        }
     }
 
     /// <inheritdoc />
     public async Task<ResultDto<CaseDto>> GetCaseAsync(CaseId caseID)
     {
-        var response = await HttpClient.GetAsync($"/api/cases/{caseID}");
-        var result = await response.Content.ReadAsAsync<ResultDto<CaseDto>>();
-        return result;
+        try
+        {
+            var response = await HttpClient.GetAsync($"/api/cases/{caseID}");
+            var result = await response.Content.ReadAsAsync<ResultDto<CaseDto>>();
+            return result;
+        }
+        catch (Exception ex)
+        {
+            return new ResultDto<CaseDto> { Code = 500, Msg = ex.Message };
+        }
     }
 
     /// <inheritdoc />
     public async Task<ResultDto<PagedListResultDto<CaseDto>>> ListPagedCasesAsync(string? reporterNo, DateTimeOffset startDate, DateTimeOffset endDate, int pageNo = 1, int pageSize = 10)
     {
-        var query = new StringBuilder();
-        query.Append($"?pageNo={pageNo}&pageSize={pageSize}&startDate={startDate:yyyy-MM-dd}&endDate={endDate:yyyy-MM-dd}");
-        if (reporterNo.IsNotNullOrEmpty())
+        try
         {
-            query.Append($"&reporterNo={reporterNo}");
+            var query = new StringBuilder();
+            query.Append($"?pageNo={pageNo}&pageSize={pageSize}&startDate={startDate:yyyy-MM-dd}&endDate={endDate:yyyy-MM-dd}");
+            if (reporterNo.IsNotNullOrEmpty())
+            {
+                query.Append($"&reporterNo={reporterNo}");
+            }
+            var response = await HttpClient.GetAsync($"/api/cases/{query}");
+            var result = await response.Content.ReadAsAsync<ResultDto<PagedListResultDto<CaseDto>>>();
+            return result;
         }
-        var response = await HttpClient.GetAsync($"/api/cases/{query}");
-        var result = await response.Content.ReadAsAsync<ResultDto<PagedListResultDto<CaseDto>>>();
-        return result;
+        catch (Exception ex)
+        {
+            return new ResultDto<PagedListResultDto<CaseDto>> { Code = 500, Msg = ex.Message };
+        }
     }
 }
--- Guardians.HttpClient/Services/SceneApplicationServiceClient.cs
+++ Guardians.HttpClient/Services/SceneApplicationServiceClient.cs
@@ -1,9 +1,11 @@
 ﻿using Fluxera.Extensions.Hosting.Modules.Application.Contracts.Dtos;
 using Fluxera.Extensions.Http;
+using Fluxera.Guards;
 using Guardians.Application.Contracts;
 using Guardians.Application.Contracts.States;
 using Guardians.Domain.Shared;
 using JetBrains.Annotations;
+using Microsoft.Extensions.Logging;
 
 namespace Guardians.HttpClient.Services;
 
@@ -18,33 +20,61 @@ internal sealed class SceneApplicationServiceClient : HttpClientServiceBase, ISc
     /// <inheritdoc />
     public async Task<ResultDto<SceneDto>> CreateSceneAsync(SceneForCreationDto input)
     {
-        var content = input.AsJsonContent();
-        var response = await HttpClient.PostAsync("/api/scenes", content);
-        var result = await response.Content.ReadAsAsync<ResultDto<SceneDto>>();
-        return result;
+        try
+        {
+            var content = input.AsJsonContent();
+            var response = await HttpClient.PostAsync("/api/scenes", content);
+            var result = await response.Content.ReadAsAsync<ResultDto<SceneDto>>();
+            return result;
+        }
+        catch (Exception ex)
+        {
+            return new ResultDto<SceneDto> { Code = 500, Msg = ex.Message };
+        }
     }
 
     /// <inheritdoc />
     public async Task<ResultDto<SceneId>> DeleteSceneAsync(SceneId sceneID)
     {
-        var response = await HttpClient.DeleteAsync($"/api/scenes/{sceneID}");
-        var result = await response.Content.ReadAsAsync<ResultDto<SceneId>>();
-        return result;
+        try
+        {
+            var response = await HttpClient.DeleteAsync($"/api/scenes/{sceneID}");
+            var result = await response.Content.ReadAsAsync<ResultDto<SceneId>>();
+            return result;
+        }
+        catch (Exception ex)
+        {
+            return new ResultDto<SceneId> { Code = 500, Msg = ex.Message };
+        }
     }
 
     /// <inheritdoc />
     public async Task<ResultDto<SceneDto>> GetSceneAsync(SceneId sceneID)
     {
-        var response = await HttpClient.GetAsync($"/api/scenes/{sceneID}");
-        var result = await response.Content.ReadAsAsync<ResultDto<SceneDto>>();
-        return result;
+        try
+        {
+            var response = await HttpClient.GetAsync($"/api/scenes/{sceneID}");
+            var result = await response.Content.ReadAsAsync<ResultDto<SceneDto>>();
+            return result;
+        }
+        catch (Exception ex)
+        {
+            return new ResultDto<SceneDto> { Code = 500, Msg = ex.Message };
+        }
     }
 
     /// <inheritdoc />
     public async Task<ResultDto<ListResultDto<SceneDto>>> ListScenesAsync()
     {
-        var response = await HttpClient.GetAsync("/api/scenes");
-        var result = await response.Content.ReadAsAsync<ResultDto<ListResultDto<SceneDto>>>();
-        return result;
+        try
+        {
+            var response = await HttpClient.GetAsync("/api/scenes");
+            var result = await response.Content.ReadAsAsync<ResultDto<ListResultDto<SceneDto>>>();
+            return result;
+        }
+        catch (Exception ex)
+        {
+            return new ResultDto<ListResultDto<SceneDto>> { Code = 500, Msg = ex.Message };
+        }
     }
 }
